644c7caca00fbcba80a06f68165a0ed368d09b33
jonathangsilveira <jonathangsilveira@gmail.com>
2019-06-06 00:28:19 -0300

    Adding new serialization dependency for JSON

--- app/src/main/java/br/edu/jgsilveira/portfolio/mvisample/api/BaseEndPoint.kt
+++ app/src/main/java/br/edu/jgsilveira/portfolio/mvisample/api/BaseEndPoint.kt
@@ -3,16 +3,22 @@ package br.edu.jgsilveira.portfolio.mvisample.api
 import br.edu.jgsilveira.portfolio.mvisample.data.Result
 import br.edu.jgsilveira.portfolio.mvisample.data.success
 import br.edu.jgsilveira.portfolio.mvisample.data.undefined
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 open class BaseEndPoint {
 
     protected val retrofit: Retrofit by lazy {
+        val contentType = MediaType.get("application/json")
         Retrofit.Builder()
             .baseUrl(Routes.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(Json.asConverterFactory(contentType = contentType))
+            .client(buildClient())
             .build()
     }
 
@@ -28,6 +34,15 @@ open class BaseEndPoint {
             Result.Failure.Response(response.code(), response.message())
     } catch (e: Exception) {
         undefined(e)
+    }
+
+    private fun buildClient(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
     }
 
 }
